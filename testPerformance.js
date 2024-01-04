813d77c8274dcfb5c1b681c48ee556586cf3fca1
Lukáš Lukča <xlukca@stuba.sk>
2024-01-04 14:19:06 +0100

    grafana

--- testPerformance.js
+++ testPerformance.js
@@ -1,10 +1,25 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
 export const options = {
-  vus: 10,
-  duration: '30s',
+  scenarios: {
+    per_vu_scenario: {
+      executor: "per-vu-iterations",
+      vus: 1,
+      iterations: 1,
+    },
+  },
 };
+
+// export const options = {
+//   discardResponseBodies: true,
+//   scenarios: {
+//     contacts: {
+//       executor: 'constant-vus',
+//       vus: 1,
+//       duration: '2m',
+//     },
+//   },
+// };
+
 export default function () {
-  http.get('http://127.0.0.1:8000/user/susdata/index');
-  sleep(1);
-}
+      http.get('http://127.0.0.1:8000/user/susdata/index');
+   }
