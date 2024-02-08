da446d411cf266da898c049e02666e720005f7de
Lukáš Lukča <xlukca@stuba.sk>
2024-02-08 17:42:29 +0100

    add link rel icon

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" type="image/x-icon" href="{{ asset('admin/img/database.png') }}" />
 
     <title>{{ __('general.database') }}</title>
 
