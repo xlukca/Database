8efbe04b886a6aa6ec14dc88de5f3259f44cb2fe
Lukáš Lukča <xlukca@stuba.sk>
2024-03-21 21:14:55 +0100

    edit SusdataSeeder

--- database/seeders/SusdataSeeder.php
+++ database/seeders/SusdataSeeder.php
@@ -99,11 +99,11 @@ class SusdataSeeder extends Seeder
             ];
         }
             // Susdata::insert($data);
-            DB::beginTransaction();
+           // DB::beginTransaction();     SQL databases
 
             try {
                 Susdata::insert($data);
-                DB::commit();
+            //    DB::commit();
             } catch (\Exception $e) {
                 // V prípade chyby rollback
                 DB::rollback();
