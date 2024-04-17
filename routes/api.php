2f61566d49c96e46c43686dae86950e03690501a
Lukáš Lukča <xlukca@stuba.sk>
2024-04-17 18:32:01 +0200

    API Susdat

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-# SUSDAT API
-Route::get('/susdats', [SusdataController::class, 'index']);
-Route::get('/susdats/{name}/', [SusdataController::class, 'show']);
+// SUSDAT API
 
-# SARS API
-Route::get('/sars', [SarsController::class, 'index']);
-Route::get('/sars/{name}', [SarsController::class, 'show']);
+    // // id
+    //     Route::get('/susdats/id/{id}/json', [SusdataController::class, 'showJSONid']);
+        Route::get('/susdats/id/{id}/xml', [SusdataController::class, 'showXMLid']);
+
+    // // name
+    //     Route::get('/susdats/name/{names}/json', [SusdataController::class, 'showJSONname']);
+        Route::get('/susdats/name/{name}/xml', [SusdataController::class, 'showXMLname']);
+
+    // // cas_rn_dashboard
+    //     Route::get('/susdats/casrn/{cas_rn_dashboard}/json', [SusdataController::class, 'showJSONcasrn']);
+        Route::get('/susdats/casrn/{cas_rn_dashboard}/xml', [SusdataController::class, 'showXMLcasrn']);
+
+    // // stdinchikey
+    //     Route::get('/susdats/stdinchikey/{stdinchikey}/json', [SusdataController::class, 'showJSONinchikey']);
+        Route::get('/susdats/stdinchikey/{stdinchikey}/xml', [SusdataController::class, 'showXMLinchikey']);
+
+    // // dtxsid
+    //     Route::get('/susdats/dtxsid/{dtxsid}/json', [SusdataController::class, 'showJSONdtxsid']);
+        Route::get('/susdats/dtxsid/{dtxsid}/xml', [SusdataController::class, 'showXMLdtxsid']);
+
+    Route::get('/susdats/{field}/{values}/json', [SusdataController::class, 'showJSON']);
+    // Route::get('/susdats/{field}/{values}/xml', [SusdataController::class, 'showXML']);
+    
+
+// SARS API
+        Route::get('/sars/{id}', [SarsController::class, 'show']);
